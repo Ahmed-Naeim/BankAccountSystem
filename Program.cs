3c39c6736c39610b9ec749f7494efc9b959141f8
Ahmed Naeim <eng.a.naeim@gmail.com>
2024-10-04 10:02:23 +0300

    editing the lists to be all account not the child class to manipulate. in stack and heap

--- Program.cs
+++ Program.cs
@@ -17,41 +17,41 @@ namespace BankAccountSystem
             AccountUtil.Withdraw(accounts, 2000);
 
             // Savings
-            var savAccounts = new List<SavingsAccount>();
+            var savAccounts = new List<Account>();
             savAccounts.Add(new SavingsAccount());
             savAccounts.Add(new SavingsAccount("Superman"));
             savAccounts.Add(new SavingsAccount("Batman", 2000));
             savAccounts.Add(new SavingsAccount("Wonderwoman", 5000, 5.0));
 
-            AccountUtil.Display(savAccounts.Cast<Account>().ToList());
-            AccountUtil.Deposit(savAccounts.Cast<Account>().ToList(), 1000);
-            AccountUtil.Withdraw(savAccounts.Cast<Account>().ToList(), 2000);
+            AccountUtil.Display(savAccounts);
+            AccountUtil.Deposit(savAccounts, 1000);
+            AccountUtil.Withdraw(savAccounts, 2000);
 
             // Checking
-            var checAccounts = new List<CheckingAccount>();
+            var checAccounts = new List<Account>();
             checAccounts.Add(new CheckingAccount());
             checAccounts.Add(new CheckingAccount("Larry2"));
             checAccounts.Add(new CheckingAccount("Moe2", 2000));
             checAccounts.Add(new CheckingAccount("Curly2", 5000));
 
-            AccountUtil.Display(checAccounts.Cast<Account>().ToList());
-            AccountUtil.Deposit(checAccounts.Cast<Account>().ToList(), 1000);
-            AccountUtil.Withdraw(checAccounts.Cast<Account>().ToList(), 2000);
-            AccountUtil.Withdraw(checAccounts.Cast<Account>().ToList(), 2000);
+            AccountUtil.Display(checAccounts);
+            AccountUtil.Deposit(checAccounts, 1000);
+            AccountUtil.Withdraw(checAccounts, 2000);
+            AccountUtil.Withdraw(checAccounts, 2000);
 
             // Trust
-            var trustAccounts = new List<TrustAccount>();
+            var trustAccounts = new List<Account>();
             trustAccounts.Add(new TrustAccount());
             trustAccounts.Add(new TrustAccount("Superman2"));
             trustAccounts.Add(new TrustAccount("Batman2", 2000));
             trustAccounts.Add(new TrustAccount("Wonderwoman2", 5000, 5.0));
 
-            AccountUtil.Display(trustAccounts.Cast<Account>().ToList());
-            AccountUtil.Deposit(trustAccounts.Cast<Account>().ToList(), 1000);
-            AccountUtil.Deposit(trustAccounts.Cast<Account>().ToList(), 6000);
-            AccountUtil.Withdraw(trustAccounts.Cast<Account>().ToList(), 2000);
-            AccountUtil.Withdraw(trustAccounts.Cast<Account>().ToList(), 3000);
-            AccountUtil.Withdraw(trustAccounts.Cast<Account>().ToList(), 500);
+            AccountUtil.Display(trustAccounts);
+            AccountUtil.Deposit(trustAccounts, 1000);
+            AccountUtil.Deposit(trustAccounts, 6000);
+            AccountUtil.Withdraw(trustAccounts, 2000);
+            AccountUtil.Withdraw(trustAccounts, 3000);
+            AccountUtil.Withdraw(trustAccounts, 500);
 
             Console.WriteLine();
         }
